1109ff71c834a1fad8ed40fdbc8b5e7f4067c627
salmazmr <111300100+salmazmr@users.noreply.github.com>
2022-11-17 03:25:15 +0700

    Add files via upload

---
+++ Movie-List-main - Copy/src/script/data/movies.js
@@ -0,0 +1,34 @@
+const movies = [
+  {
+    name: 'The Godfather',
+    genre: 'Drama, Crime',
+    poster: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/3bhkrj58Vtu7enYsRolD1fZdja1.jpg',
+    overview: 'Spanning the years 1945 to 1955, a chronicle of the fictional Italian-American Corleone crime family. When organized crime family patriarch, Vito Corleone barely survives an attempt on his life, his youngest son, Michael steps in to take care of the would-be killers, launching a campaign of bloody revenge.',
+  },
+  {
+    name: 'Thor: Love and Thunder',
+    genre: 'Fantasy, Action, Comedy',
+    poster: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/pIkRyD18kl4FhoCNQuWxWu5cBLM.jpg',
+    overview: 'After his retirement is interrupted by Gorr the God Butcher, a galactic killer who seeks the extinction of the gods, Thor Odinson enlists the help of King Valkyrie, Korg, and ex-girlfriend Jane Foster, who now wields Mjolnir as the Mighty Thor. Together they embark upon a harrowing cosmic adventure to uncover the mystery of the God Butcher’s vengeance and stop him before it’s too late.',
+  },
+  {
+    name: 'Top Gun: Maverick',
+    genre: 'Action, Drama',
+    poster: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/62HCnUTziyWcpDaBO2i1DX17ljH.jpg',
+    overview: 'After more than thirty years of service as one of the Navy’s top aviators, and dodging the advancement in rank that would ground him, Pete “Maverick” Mitchell finds himself training a detachment of TOP GUN graduates for a specialized mission the likes of which no living pilot has ever seen.',
+  },
+  {
+    name: 'The Black Phone',
+    genre: 'Horror, Thriller',
+    poster: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/lr11mCT85T1JanlgjMuhs9nMht4.jpg',
+    overview: 'Finney Blake, a shy but clever 13-year-old boy, is abducted by a sadistic killer and trapped in a soundproof basement where screaming is of little use. When a disconnected phone on the wall begins to ring, Finney discovers that he can hear the voices of the killer’s previous victims. And they are dead set on making sure that what happened to them doesn’t happen to Finney.',
+  },
+  {
+    name: 'The Shawshank Redemption',
+    genre: 'Drama, Crime',
+    poster: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg',
+    overview: 'Framed in the 1940s for the double murder of his wife and her lover, upstanding banker Andy Dufresne begins a new life at the Shawshank prison, where he puts his accounting skills to work for an amoral warden. During his long stretch in prison, Dufresne comes to be admired by the other inmates -- including an older prisoner named Red -- for his integrity and unquenchable sense of hope.',
+  }
+];
+
+export default movies;
